619857baeeaee9c3c107ae902d5f0611caacb647
Isabel Zavala <isazaba278@gmail.com>
2025-04-02 20:32:00 -0600

    Final de proyecto

---
+++ SumaDosNumeros.js
@@ -0,0 +1,7 @@
+var num1 = Number(prompt('Inserta el primer numero: '));
+var num2 = Number(prompt('Inserta el segundo numero: '));
+var sum = num1 + num2;
+
+
+
+alert('La suma de los numeros es : ' + num1 + ' + ' + num2 + ' = ' + sum);
